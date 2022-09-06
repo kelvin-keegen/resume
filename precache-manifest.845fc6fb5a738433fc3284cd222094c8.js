7fc7e4baa3f0d111fe8876aec167f9a3fe7fe554
Kelvin Keegan <kelvinkeegen17@gmail.com>
2022-09-06 19:06:07 +0100

    Updates

---
+++ precache-manifest.845fc6fb5a738433fc3284cd222094c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29c0ba4c38449846d6fa",
+    "url": "/resume/static/css/main.cb35f215.chunk.css"
+  },
+  {
+    "revision": "29c0ba4c38449846d6fa",
+    "url": "/resume/static/js/main.29c0ba4c.chunk.js"
+  },
+  {
+    "revision": "5154ca97eaf86c0f4660",
+    "url": "/resume/static/js/1.5154ca97.chunk.js"
+  },
+  {
+    "revision": "089974fd6daca93e2e66",
+    "url": "/resume/static/js/2.089974fd.chunk.js"
+  },
+  {
+    "revision": "62c5afb06a0ab88eab51",
+    "url": "/resume/static/js/runtime~main.62c5afb0.js"
+  },
+  {
+    "revision": "71dc492ebe7bb2b50e8e182bcd9c1621",
+    "url": "/resume/index.html"
+  }
+];
